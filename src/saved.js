020fc70fc60ae2b424369aef7e265a6251b4cbd8
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-03-29 10:44:17 -0700

    psuedo code for js

---
+++ src/saved.js
@@ -0,0 +1,6 @@
+// Want to create a select drop down with VIN values from firebase
+// Use the Branch ref (VIN) under vin-data ref to populate options
+// When select option is submitted, access the required vin ref
+// Adapt create-vin-list function an call it within a forEach loop.
+// all of this code should be within auth.onAuthStateChanged
+// also need to use .on
